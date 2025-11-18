804d7b1234db19d056466c23ffcd66e4612a244a
sonlar <31799749+sonlar@users.noreply.github.com>
2025-11-18 23:15:17 +0100

    Implement method for extracting keywords out of a text

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import keyword_spacy
+import os
 import pymupdf
+import spacy
 class Build_Corpus:
     def find_files(self, path: str) -> list:
         return os.listdir(path)
@@ -9,3 +12,8 @@ class Build_Corpus:
         for page in reader:
             doc += str(page.get_text())
         return doc
+    def extract_keywords(self, text: str ) -> list:
+        nlp = spacy.load("en_core_web_md")
+        nlp.add_pipe("keyword_extractor", last=True, config={"top_n": 10, "min_ngram": 3, "max_ngram": 3, "strict": True})
+        doc = nlp(text)
+        return [word[0] for word in doc._.keywords]
