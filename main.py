886e0b8afb107f84b16e1a2298e35c93805bab73
sonlar <31799749+sonlar@users.noreply.github.com>
2025-11-18 23:31:54 +0100

    Rename some methods and write up future plan

--- main.py
+++ main.py
@@ -2,33 +2,39 @@ import keyword_spacy
 import os
 import pymupdf
 import spacy
+
+
 class Build_Corpus:
-    def find_files(self, path: str) -> list:
+    def __find_files(self, path: str) -> list:
         return os.listdir(path)
 
-    def read_pdf(self, document: str) -> str:
+    def __read_pdf(self, document: str) -> str:
         reader = pymupdf.open(document)
         doc = str()
         for page in reader:
             doc += str(page.get_text())
         return doc
-    def extract_keywords(self, text: str ) -> list:
+
+    def __extract_keywords(self, text: str ) -> list:
         nlp = spacy.load("en_core_web_md")
-        nlp.add_pipe("keyword_extractor", last=True, config={"top_n": 10, "min_ngram": 3, "max_ngram": 3, "strict": True})
+        nlp.add_pipe("keyword_extractor", last=True, config={"top_n": 5, "min_ngram": 1, "max_ngram": 3, "strict": True})
         doc = nlp(text)
         return [word[0] for word in doc._.keywords]
 
-    def engine(self, path: str) -> None:
-        files = self.find_files(path)
+    def extract(self, path: str) -> None:
+        files = self.__find_files(path)
         for file in files:
-            text = self.read_pdf(path+file)
-            keywords = self.extract_keywords(text)
-            print(f"{file}: {keywords}")
+            text = self.__read_pdf(path+file)
+            keywords = self.__extract_keywords(text)
+            print(f"{file}: {keywords}\n")
 
 
+# TODO: Implement chroma db
+# TODO: Find appropriate model on huggingface for RAG based on corpus extracted from pdfs
 
 if __name__ == "__main__":
     corpo = Build_Corpus()
-    files = corpo.engine(path="./data/")
+    corpo_extract = corpo.extract(path="./data/")
+
     
 
