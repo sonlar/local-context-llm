20855836065cf2ff05138c1581e36d315bd6b3ae
sonlar <31799749+sonlar@users.noreply.github.com>
2025-11-18 23:15:49 +0100

    Implement engine for calling the different methods

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ class Build_Corpus:
         nlp.add_pipe("keyword_extractor", last=True, config={"top_n": 10, "min_ngram": 3, "max_ngram": 3, "strict": True})
         doc = nlp(text)
         return [word[0] for word in doc._.keywords]
+
+    def engine(self, path: str) -> None:
+        files = self.find_files(path)
+        for file in files:
+            text = self.read_pdf(path+file)
+            keywords = self.extract_keywords(text)
+            print(f"{file}: {keywords}")
+
+
+
+if __name__ == "__main__":
+    corpo = Build_Corpus()
+    files = corpo.engine(path="./data/")
+    
+
